30c0b452870cb6f361a45826235f25c0584b4550
Thilina <thilinasandaru1@gmail.com>
2019-06-15 10:18:32 +0530

    update login function

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -22,20 +22,70 @@ class AuthController extends Controller
 
     public function login()
     {
-        $client = DB::table('oauth_clients')
-            ->where('password_client', true)
-            ->first();
+       // check if a user with the specified email exists
+       $user = User::whereEmail(request('username'))->first();
 
-        $data = [
-            'grant_type' => 'password',
-            'client_id' => $client->id,
-            'client_secret' => $client->secret,
-            'username' => request('username'),
-            'password' => request('password')
-        ];
+       if(!$user)
+       {
+           return response()->json([
+               'message' => 'Wrong email or password',
+               'status' => 422
+           ], 422);
+       }
 
-        $request = Request::create('/oauth/token', 'POST', $data);
-        return app()->handle($request);
+       // If a user with the email was found - check if the specified password
+       // belongs to this user
+       if(!Hash::check(request('password'), $user->password))
+       {
+           return response()->json([
+               'message' => 'Wrong email or password',
+               'status' => 422
+           ], 422);
+       }
+
+       // Send an internal API request to get an access token
+       $clinet = DB::table('oauth_clients')
+        ->where('password_client', true)
+        ->first();
+
+       // Make sure a password client exists in the DB
+       if(!$client)
+       {
+           return response()->json([
+               'message' => 'Laravel Passport is not setup properly.',
+               'status' => 500
+           ], 500);
+       }
+
+       $data = [
+           'grant_type' => 'password',
+           'client_id' => $client->id,
+           'client_secret' => $client->secret,
+           'username' => request('username'),
+           'password' => request('password')
+       ];
+
+       $request = Request::create('/oauth/token', 'POST', $data);
+       $response = app()->handle($request);
+
+       // check if the request was successful
+       if($response->getStatusCode() != 200)
+       {
+           return response()->json([
+               'message' => 'Wrong email or password',
+               'status' => 422
+           ], 422);
+       }
+
+       // Get the data from the response
+       $data = json_decode($response->getContent());
+
+       // Format the final response in a desirable format
+       return response()->json([
+           'token' => $data->access_token,
+           'user' => $user,
+           'status' => 200
+       ]);
     }
 
     public function logout()
